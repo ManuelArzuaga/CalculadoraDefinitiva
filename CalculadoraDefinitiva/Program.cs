aa9a902170533a7125d47b1069675b3ec9d2cef3
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2025-02-27 10:16:14 -0300

    Agregar archivos de proyecto.

---
+++ CalculadoraDefinitiva/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraDefinitiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
